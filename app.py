04af631a4ce04fcd141bb069a58d4147eecbc8ac
AgiroUsasa <maximiliano.z.acunia@gmail.com>
2024-10-22 10:33:03 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 
 # Cargar el modelo YOLOv8
 model = YOLO('detect.v1i.yolov8/test.pt')  # Ruta a tu modelo entrenado
+@app.route('/')
+def index():
+    return send_from_directory('red_neuronal.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -36,8 +39,22 @@ def predict():
             x1, y1, x2, y2 = box.xyxy[0]  # Coordenadas de la caja delimitadora
             confidence = box.conf[0]      # Confianza de la predicción
             class_id = int(box.cls[0])    # ID de clase
+
+            # Aquí puedes mapear class_id a tus nombres de clase
+            class_names = [
+                "Apple_lle",
+                "AppleUSB_Mause",
+                "Commodore64",
+                "ContexCalculators",
+                "GeniusGM_6000",
+                "IBM3348",
+                "MSX_DPC-200",
+                "PowerBook160"
+            ]
+            class_name = class_names[class_id] if class_id < len(class_names) else "Unknown"
+
             detecciones.append({
-                'class_id': class_id,
+                'class_name': class_name,  # Cambié class_id a class_name
                 'confidence': confidence.item(),
                 'box': [x1.item(), y1.item(), x2.item(), y2.item()]
             })
