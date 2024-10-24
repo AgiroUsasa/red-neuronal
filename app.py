b3a3ed86abdb90911eb5e9f044fc97dd30da0ce7
AgiroUsasa <maximiliano.z.acunia@gmail.com>
2024-10-24 10:12:43 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'image' not in request.json:
-        return jsonify({'error': 'No image provided'}), 400
+        return jsonify({'error': 'No se proporcionó una imagen'}), 400
+    
     data = request.json['image']
     # Decodificar la imagen
     img_data = base64.b64decode(data.split(',')[1])
@@ -38,29 +39,26 @@ def predict():
     detecciones = []
     for result in results:
         for box in result.boxes:
-            x1, y1, x2, y2 = box.xyxy[0]  # Coordenadas de la caja delimitadora
-            confidence = box.conf[0]      # Confianza de la predicción
-            class_id = int(box.cls[0])    # ID de clase
+            x1, y1, x2, y2 = box.xyxy[0]
+            confidence = box.conf[0]
+            class_id = int(box.cls[0])
 
-            # Aquí puedes mapear class_id a tus nombres de clase
             class_names = [
-                "Apple_lle",
-                "AppleUSB_Mause",
-                "Commodore64",
-                "ContexCalculators",
-                "GeniusGM_6000",
-                "IBM3348",
-                "MSX_DPC-200",
-                "PowerBook160"
-                "CuboMirro"
+                "Apple_lle", "AppleUSB_Mause", "Commodore64", "ContexCalculators", 
+                "GeniusGM_6000", "IBM3348", "MSX_DPC-200", "PowerBook160", "CuboMirro"
             ]
-            class_name = class_names[class_id] if class_id < len(class_names) else "Unknown"
+            class_name = class_names[class_id] if class_id < len(class_names) else "Desconocido"
 
             detecciones.append({
-                'class_name': class_name,  # Cambié class_id a class_name
+                'class_name': class_name,
                 'confidence': confidence.item(),
                 'box': [x1.item(), y1.item(), x2.item(), y2.item()]
             })
+    # Enviar una notificación en el JSON cuando hay detecciones
+    if detecciones:
+        return jsonify({'detecciones': detecciones, 'message': 'Se ha detectado una imagen con éxito'})
+    else:
+        return jsonify({'detecciones': [], 'message': 'No se detectaron objetos'})            
 
     # Eliminar la imagen temporal si no es necesaria
     os.remove(img_path)
